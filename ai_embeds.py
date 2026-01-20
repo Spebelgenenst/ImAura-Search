6fca8d85277e839da84c63c55fb37f2ffb6a92a3
Spebell <sebastian@die-sperlinge.de>
2026-01-20 13:40:49 +0100

    removed with torch.no_grad():

--- ai_embeds.py
+++ ai_embeds.py
@@ -17,10 +17,9 @@ class embeds():
 
         inputs = processor(images=image, return_tensors="pt")
 
-        with torch.no_grad():
-            image_embeds = model.get_image_features(
-                pixel_values=inputs['pixel_values'],
-            )
+        image_embeds = model.get_image_features(
+            pixel_values=inputs['pixel_values'],
+        )
 
         return image_embeds
 
@@ -28,11 +27,10 @@ class embeds():
     def text_embed(self, text):
         inputs = processor(text=text, return_tensors="pt")
 
-        with torch.no_grad():
-            text_embeds = model.get_text_features(
-                input_ids=inputs['input_ids'],
-                attention_mask=inputs['attention_mask'],
-                token_type_ids=inputs['token_type_ids'],
-            )
+        text_embeds = model.get_text_features(
+            input_ids=inputs['input_ids'],
+            attention_mask=inputs['attention_mask'],
+            token_type_ids=inputs['token_type_ids'],
+        )
 
 embeds()
